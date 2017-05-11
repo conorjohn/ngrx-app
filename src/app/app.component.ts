9bb2a1501dd440357cd53aa9991e04ad756b9f31
Conor John O'Reilly <conorj92@gmail.com>
2017-05-11 23:05:40 +0100

    getting started on ngrx/observables course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  clock = Observable.interval(1000)
+    .map(() => new Date());
 }
